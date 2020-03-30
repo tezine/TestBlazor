c243c3166831b68f422f430786636600958803a4
Bruno Tezine <bruno@tezine.com>
2020-03-30 14:03:36 -0300

    first version

---
+++ Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestBlazor.Controllers {
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase {
+        [HttpGet("GetTest")]
+        public IActionResult GetTest() {
+            return Ok("OK");
+        }
+    }
+}
